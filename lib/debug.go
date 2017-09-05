66741a4837f49839d060e8ab22fc8b64a0058dcd
Kalle Kankare <kalle.kankare@iki.fi>
2017-09-05 15:39:05 +0300

    cmdlog: add debugTrace

---
+++ lib/debug.go
@@ -0,0 +1,29 @@
+package cmdlib
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	debugLogFile = os.Getenv("CMDLOG_DEBUG_TRACE")
+)
+
+func debugTrace(args ...interface{}) {
+	if debugLogFile == "" {
+		return
+	}
+	f, err := os.OpenFile(debugLogFile, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		panic(fmt.Sprint("Debug file opening failed:", err))
+	}
+	defer f.Close()
+
+	args = append(args, time.Now().String())
+
+	_, err = fmt.Fprintln(f, args...)
+	if err != nil {
+		panic(fmt.Sprint("Appending to debug file failed", err))
+	}
+}
