30b78007c4a5285caf24bdc8d6832622b3b7a301
Kalle Kankare <kalle.kankare@iki.fi>
2017-10-12 21:01:00 +0300

    cmdlog: fix log test

--- lib/log_test.go
+++ lib/log_test.go
@@ -11,7 +11,7 @@ func TestAppendLine(t *testing.T) {
 	var buf bytes.Buffer
 
 	session := "ses"
-	cmd := []string{"first", "second"}
+	cmd := "first second"
 
 	AppendLine(&buf, session, cmd)
 	items := strings.Split(buf.String(), "\t")
@@ -29,24 +29,24 @@ func TestAppendLine(t *testing.T) {
 		t.Error("Improper session", items[1], "!=", session)
 	}
 
-	if strings.TrimRight(items[2], "\n") != strings.Join(cmd, " ") {
+	if strings.TrimRight(items[2], "\n") != cmd {
 		t.Error("Command is improperly concatenated:", items[2],
-			"Opposed to", strings.Join(cmd, " "))
+			"Opposed to", cmd)
 	}
 }
 
 func TestAppendLongLine(t *testing.T) {
 	var buf bytes.Buffer
 
-	var cmd []string
+	var cmd string
 	count := 32 * 1024
 	str := "KEKE"
 	for i := 0; i < count; i++ {
-		cmd = append(cmd, str)
+		cmd = cmd + " " + str
 	}
 	AppendLine(&buf, "Jeps", cmd)
 	items := strings.SplitN(buf.String(), "\t", 3)
-	length := count*len(str) + count + 1 - 1
+	length := count*len(str) + count + 1
 	if len(items[2]) != length {
 		t.Error("Length of the command line is", len(items[2]),
 			"should be", length)
